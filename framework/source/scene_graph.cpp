6fe975d82c2ac58ce5120850c1b81ba812685df2
Mielein <marie.trojan@uni-weimar.de>
2021-10-26 21:13:56 +0200

    added scene_graph.cpp

---
+++ framework/source/scene_graph.cpp
@@ -0,0 +1,26 @@
+#include "scene_graph.hpp"
+
+class scene_graph{
+    std::string name = " ";
+    Node root;
+
+    std::string getName(){
+        return name;
+    }
+    void setName(std::string n){
+        name = n;
+    }
+
+    Node getRoot(){
+        return root;
+    }
+
+    void setRoote(Node r){
+        root = r;
+    }
+
+    std::string printGraph(){
+        //TODO
+    }
+
+};

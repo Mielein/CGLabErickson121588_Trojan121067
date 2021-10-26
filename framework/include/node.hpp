0b52e6813ad8257030dd20e65ed8663829f162b7
YoFionn <ericksoncooperation@gmail.com>
2021-10-26 21:30:13 +0200

    node hpp

--- framework/include/node.hpp
+++ framework/include/node.hpp
@@ -1,19 +1,35 @@
 #ifndef APPLICATION_HPP
 #define APPLICATION_HPP
 
+#include "node.hpp"
+
 #include <glm/gtc/type_precision.hpp>
+#include <string>
+#include <list>
 
 class Node{
     private:
         Node parent;
-        List<Node> children;
-        string name;
-        string path;
+        std::list<Node> children;
+        std::string name;
+        std::string path;
         int depth;
-        mat4 localTransform;
-        mat4 worldTransform;
+        glm::mat4 localTransform;
+        glm::mat4 worldTransform;
     public:
-        getPare
-}
+        Node getParent();
+        void setParent(Node node);
+        Node getChildren(std::string name);
+        std::list<Node> getChildrenList();
+        std::string getName();
+        std::string getPath();
+        int getDepth();
+        glm::mat4 getLocalTransform();
+        void setLocalTransform(glm::mat4);
+        glm::mat4 getWorldTransform();
+        void setWorldTransform(glm::mat4);
+        void addChildren(Node);
+        Node removeChildren(std::string);
+};
 
 #endif

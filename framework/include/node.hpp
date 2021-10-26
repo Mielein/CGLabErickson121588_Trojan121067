63f0f7de8016abf18a0843de35a6f6a4064aa43d
YoFionn <ericksoncooperation@gmail.com>
2021-10-26 20:50:26 +0200

    added a Node class

---
+++ framework/include/node.hpp
@@ -0,0 +1,19 @@
+#ifndef APPLICATION_HPP
+#define APPLICATION_HPP
+
+#include <glm/gtc/type_precision.hpp>
+
+class Node{
+    private:
+        Node parent;
+        List<Node> children;
+        string name;
+        string path;
+        int depth;
+        mat4 localTransform;
+        mat4 worldTransform;
+    public:
+        getPare
+}
+
+#endif

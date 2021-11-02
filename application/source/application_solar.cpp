ac8f0ada671f2e320f2dacf1c613010e34be57b9
YoFionn <ericksoncooperation@gmail.com>
2021-11-02 11:18:31 +0100

    some debug output

--- application/source/application_solar.cpp
+++ application/source/application_solar.cpp
@@ -41,6 +41,9 @@ void ApplicationSolar::render() const {
   // bind shader to upload uniforms
   glUseProgram(m_shaders.at("planet").handle);
   // scene_graph_.printClass();
+  Node tmp_node("First element");
+  scene_graph_.getRoot().addChildren(tmp_node);
+  scene_graph_.getRoot().getChild();
 
   glm::fmat4 model_matrix = glm::rotate(glm::fmat4{}, float(glfwGetTime()), glm::fvec3{0.0f, 0.0f, 1.0f});
   model_matrix = glm::translate(model_matrix, glm::fvec3{0.0f, 0.0f, -1.0f});

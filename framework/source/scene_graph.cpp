9cd53c9e8ca9cc01d6c0be2dbd674e6f3838eb32
Mielein <marie.trojan@uni-weimar.de>
2021-11-03 11:12:16 +0100

    prints parents now but earth twice

--- framework/source/scene_graph.cpp
+++ framework/source/scene_graph.cpp
@@ -37,7 +37,7 @@ void Scene_graph::setRoot(Node r){
 std::string printChildren(std::vector<std::shared_ptr<Node>> children){
     std::string debug_return;
     for(auto const& x : children){
-        std::cout << x->getName() << " -> "; //ToDo (not that much more) the format is wrong but else... ITS DONE
+        std::cout << x->getParent()->getName()<< " -> " << x->getName() << " -> "; //ToDo (not that much more) the format is wrong but else... ITS DONE
         if(x->getChildrenList().size() != 0){
             printChildren(x->getChildrenList());
         }
